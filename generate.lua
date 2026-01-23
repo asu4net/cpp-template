5bae8018d2668adfba76782dde4464605693dace
Alejandro Suárez Perales <asu4net@gmail.com>
2026-01-23 20:15:21 +0100

    Improved prj config

--- generate.lua
+++ generate.lua
@@ -1,79 +1,93 @@
-Render_Backend = {
-    D3D11 = "GAME_D3D11",
+---------------------------------------------
+------------ GLOBAL VARIABLES ---------------
+---------------------------------------------
+
+-- @Note: Basic configs:
+GPU_API = {
     GL = "GAME_GL"
 }
 
---------- GLOBAL VARIABLES ---------
+cpp_version = "C++17"
+gpu_api     = GPU_API.GL
 
-render_backend    = Render_Backend.GL
-cpp_version       = "C++17"
-binaries_dir      = "%{wks.location}/../.bin/%{prj.name}/%{cfg.buildcfg}"
-intermediates_dir = "%{wks.location}/../.bin-int/%{prj.name}/%{cfg.buildcfg}"
+-- @Note: Dir locations:
+loc_this  = "./" -- @Note: You can change this if you want to move generate.lua.
+loc_bin   = loc_this .. ".bin/%{prj.name}/%{cfg.buildcfg}"
+loc_int   = loc_this .. ".bin-int/%{prj.name}/%{cfg.buildcfg}"
+loc_core  = loc_this .. "core/" 
+loc_3rd   = loc_this .. "3rd/"
+loc_imgui = loc_3rd .. "imgui/"
 
---------- BACKEND UTILITIES ---------
+-- @Note: Include dirs:
 
-function is_windows()
-    return package.config:sub(1,1) == '\\'
-end
+-- imgui:
+inc_imgui = {
+    loc_imgui .. "src/",
+    loc_imgui .. "src/backends/",   
+}
 
-function defines_backend()
-    filter {}
-    defines { render_backend }
-    if not is_windows() and render_backend == Render_Backend.D3D11 then
-        undefines { render_backend }
-        print("Dude, d3d11 is just for windows.")
-    end
-end
+-- 3rd:
+inc_3rd = {
+    inc_imgui,
+    loc_3rd .. "miniaudio",
+    loc_3rd .. "tiny_obj_loader",
+    loc_3rd .. "khr",
+    loc_3rd .. "stb",
+}
 
-function link_backend()
-    filter "system:windows"
-        links { "User32.lib" }
-    filter {}
-    if is_windows() and render_backend == Render_Backend.D3D11 then
-        filter "system:windows"
-            links { "d3d11.lib" }
-        elseif  render_backend == Render_Backend.GL then
-            links { "OpenGL32.lib" }
-        end
-    filter {}
-end
+-- core:
+inc_core = {
+    inc_3rd,
+    loc_core,
+    loc_core .. "**",
+}
 
---------- PROJECT UTILITIES ---------
+-- any game:
 
-function cpp_project(name)
-    project(name)
-    language "C++"
-    cppdialect (cpp_version)
-    targetdir (binaries_dir)
-    objdir (intermediates_dir)
-    debugdir ("%{wks.location}/" .. name)
-    
-    -- @Note: Named PCH approach.
-    -- if use_pch ~= false then
-    --    local pch = name .. "_pch"
-    --    pchheader (pch .. ".h")
-    --    pchsource (pch_dir .. pch .. ".cpp")
-    --    forceincludes (pch .. ".h")
-    
-end
+inc_game_base = {
+    inc_3rd,
+    inc_core,
+}
 
-function cpp_lib_project(name)
-    cpp_project(name)
-    kind "StaticLib"
-end
+-- @Note: Files
 
-function cpp_app_project(name)
-    cpp_project(name)
-    filter "configurations:dist"
-        kind "WindowedApp"
-        entrypoint "mainCRTStartup"
-    filter "configurations:debug or configurations:release"
-        kind "ConsoleApp"
-    filter {}
-end
+-- core:
+fls_core = {
+    loc_3rd .. "miniaudio/**.h", 
+    loc_3rd .. "tiny_obj_loader/**.h", 
+    loc_3rd .. "khr/**.h", 
+    loc_3rd .. "stb/**.h", 
+    loc_core .. "**.h",
+    loc_core .. "**.cpp",  
+}
 
+-- @Note: Links
+
+-- any game:
+lnk_game_base = {
+    "imgui",
+    "core",
+}
+
+lnk_game_win = {
+    "User32.lib",
+    "OpenGL32.lib",
+    lnk_game_base,
+}
+
+---------------------------------------------
+------------ PROJECT FUNCTIONS --------------
+---------------------------------------------
 function config_base()
 
+
+    filter {}
+        defines {
+            gpu_api,
+            "_CRT_SECURE_NO_WARNINGS",
+            "_SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING"
+        }
+    
     filter "configurations:debug"
         runtime  "Debug"
         symbols  "On"
@@ -94,31 +108,71 @@ function config_base()
 
     filter "system:windows"
         defines "GAME_WIN"
+    
+    filter {}
 end
 
--------------------------------------------------
+function prj_base(name)
+    project(name)
+    language "C++"
+    cppdialect (cpp_version)
+    targetdir (loc_bin)
+    objdir (loc_int)
+        
+end
 
---    @Pending: We would need to do something like this in the future, for both core and graphics.
---
---    filter {}
---
---    -- First remove all backend files
---    removefiles { "src/core/backend/**.h", "src/core/backend/**.cpp" }
---
---    -- Then add render backend files
---    if render_backend == Render_Backend.D3D11 then
---        files { "src/core/backend/d3d11/**.h", "src/core/backend/d3d11/**.cpp" }
---    elseif render_backend == Render_Backend.GL then
---        files { "src/core/backend/opengl/**.h", "src/core/backend/opengl/**.cpp" }
---    end
---
---    -- Also add platform specific files 
---    filter "system:windows"
---        files { "src/core/backend/win32/**.h", "src/core/backend/win32/**.cpp" }
---    filter {}
--- end
+function prj_lib(name)
+    prj_base(name)
+    kind "StaticLib"
+end
 
---------- WORKSPACE ---------
+function prj_app(name)
+    prj_base(name)
+    debugdir (loc_this .. name)
+    filter "configurations:dist"
+        kind "WindowedApp"
+        entrypoint "mainCRTStartup"
+    filter "configurations:debug or configurations:release"
+        kind "ConsoleApp"
+    filter {}
+end
+
+function prj_game(name)
+    prj_app(name)
+
+    --- @Note: PCH config
+    pchheader "pch.h"
+    pchsource (name .. "/pch.cpp")
+    forceincludes "pch.h"
+    ---
+    config_base()
+
+    local loc = loc_this .. name 
+
+    includedirs { 
+        inc_game_base, 
+        loc .. "/code",
+    }
+
+    files { 
+        loc .. "/pch.h", 
+        loc .. "/pch.cpp",
+        loc .. "/code/**.h", 
+        loc .. "/code/**.cpp", 
+        loc .. "/assets/**.glsl", 
+    }
+
+    filter "system:windows"
+        links(lnk_game_win)
+end
+
+---------------------------------------------
+-------------- CONFIGURATION ----------------
+---------------------------------------------
+
+---------------------------------------------
+---------------- WORKSPACE ------------------
+---------------------------------------------
 
 workspace "cpp-template"
     location ".project-files"
@@ -126,68 +180,49 @@ workspace "cpp-template"
     configurations { "debug", "release", "dist" }
     startproject "sandbox"
 
+---------------------------------------------
+-------------- PROJECT IMGUI ----------------
+---------------------------------------------
+
 group "3rd"
-    include "3rd/imgui/imgui_generate.lua"
+    prj_lib "imgui"
+        config_base()
+        includedirs (inc_imgui)
+        files { 
+            loc_imgui .. "src/**.h", 
+            loc_imgui .. "src/**.cpp", 
+        }
+        
+        removefiles (loc_imgui .. "src/backends/**")
+        
+        filter "system:windows"
+            files 
+            { 
+                loc_imgui .. "src/backends/imgui_impl_opengl3_loader.h", 
+                loc_imgui .. "src/backends/imgui_impl_opengl3.h", 
+                loc_imgui .. "src/backends/imgui_impl_opengl3.cpp", 
+                loc_imgui .. "src/backends/imgui_impl_win32.h", 
+                loc_imgui .. "src/backends/imgui_impl_win32.cpp" 
+            }
+        filter{}
 group ""
 
-loc_core = "%{wks.location}/../core/" 
-loc_3rd = "%{wks.location}/../3rd/"
+---------------------------------------------
+-------------- PROJECT CORE -----------------
+---------------------------------------------
 
-core_includedirs = {
-    loc_3rd .. "miniaudio",
-    loc_3rd .. "tiny_obj_loader",
-    loc_3rd .. "khr",
-    loc_3rd .. "stb",
-    loc_3rd .. "imgui/src/",
-    loc_3rd .. "imgui/src/backends/", 
-    loc_core,
-    loc_core .. "**",
-}
-
-cpp_lib_project "core"
-    pchheader ("core_pch.h")
-    pchsource ("core/core_pch.cpp")
-    forceincludes ("core_pch.h")
-    includedirs(core_includedirs)
-    
-    files 
-    {
-        loc_3rd .. "miniaudio/**.h", 
-        loc_3rd .. "tiny_obj_loader/**.h", 
-        loc_3rd .. "khr/**.h", 
-        loc_3rd .. "stb/**.h", 
-        loc_core .. "/**.h",
-        loc_core .. "/**.cpp",
-    }
-    
+prj_lib "core"
+    -- @Note: PCH config
+    pchheader "core_pch.h"
+    pchsource (loc_core .. "core_pch.cpp")
+    forceincludes "core_pch.h"
+    --
     config_base()
-    defines_backend()
+    includedirs(inc_core)
+    files(fls_core)
 
-function use_core()
-    includedirs(core_includedirs)
-end
+---------------------------------------------
+--------------- GAME SANDBOX ----------------
+---------------------------------------------
 
-function link_core()
-    links { "core", "imgui" }
-    defines
-    {
-        "_CRT_SECURE_NO_WARNINGS",
-        "_SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING"
-    }
-end
-
-function config_game()
-    
-    files { "assets/**.glsl" }    
-
-    use_core()
-    
-    config_base()
-    defines_backend()
-
-    link_backend()
-    link_core()
-
-end
-
-include "sandbox/sandbox_generate.lua"
+prj_game "sandbox"
